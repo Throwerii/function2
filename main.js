996ae72f48f405e99f770b40ad46c05664a22950
Joe Thrower <jmthrower2@hotmail.com>
2020-05-26 14:19:20 -0400

    started on exp

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let sumN = document.querySelector("#sum")
 let multiplyButton = document.querySelector("#multiply")
 let product = document.querySelector("#product")
 let powerButton = document.querySelector("#power")
-let exponent = document.querySelector("#exp")
+let power = document.querySelector("#exp")
 let factorButton = document.querySelector("#factor")
 let factor = document.querySelector("#fact")
 addButton.addEventListener("click", function(){
@@ -28,18 +28,60 @@ multiplyButton.addEventListener("click", function(){
         return a+b
     }
     function multiply(a,b){
-        let numberOne=one
-        let numberTwo=two
-        for(let i=0;i<=numberTwo;i+=1){
-            let currentNumber=numberOne
-            let newNumber= add(currentNumber,numberOne)
+    
+        var currentNumber=one
+        for(let i=2;i<=two;i+=1){
+            let newNumber= add(one,currentNumber)
             currentNumber=newNumber
-            console.log(newNumber)
-            return newNumber
+            console.log(newNumber,currentNumber,i)
+           
+          
         }
+        return currentNumber
     }
     console.log("Multiply button clicked")
     
     // console.log(multiply(one,two))
     product.innerHTML=multiply(one,two)
 })
+powerButton.addEventListener("click",function(){
+    const one=parseInt(firstNumber.value)
+    const two=parseInt(secondNumber.value)
+    
+    function add(a,b){
+        return a+b
+    }
+    function multiply(a,b){
+    
+        var currentNumber=one
+        for(let i=2;i<=two;i+=1){
+            let newNumber= add(one,currentNumber)
+            currentNumber=newNumber
+            // console.log(newNumber,currentNumber,i)
+           
+          
+        }
+        return currentNumber
+    }
+    function exp(a,b){
+    
+        let currentNumber=one
+        console.log(currentNumber)
+        for(let i=1;i<=two;i+=1){
+            var newNumber= multiply(one,currentNumber)
+            currentNumber=newNumber
+            console.log(newNumber,currentNumber,i)
+           
+          
+        }
+        return currentNumber
+    }
+    console.log("POOOOWWWEEERRRR!!!! button clicked")
+    
+    // console.log(multiply(one,two))
+    power.innerHTML=exp(one,two)
+})
+// factorButton.addEventListener("click",function(){
+    // const one=parseInt(firstNumber.value)
+    // const two=parseInt(secondNumber.value)
+// })
